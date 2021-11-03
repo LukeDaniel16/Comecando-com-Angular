fe3361ff088983976bc9c3d2869069dc2ebd2b4a
LukeDaniel16 <lucasfelepe@gmail.com>
2021-11-03 13:20:51 -0300

    Criando form básico com ReactiveFormsModule

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
@@ -9,8 +10,16 @@ import { Todo } from 'src/models/todo.model';
 export class AppComponent {
   public todos: Todo[] = [];
   public title: String = 'Minhas tarefas';
+  public form: FormGroup;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
     this.todos.push(new Todo(1, 'Passear com o cachorro', false));
     this.todos.push(new Todo(2, 'Ir à academia', false));
     this.todos.push(new Todo(3, 'Ir ao supermercado', false));
@@ -27,10 +36,10 @@ export class AppComponent {
   }
 
   maskAsDone(todo: Todo) {
-
+    todo.done = true;
   }
 
   markAsUndone(todo: Todo) {
-
+    todo.done = false;
   }
 }
